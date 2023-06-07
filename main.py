d1200b430d792770cac8fdeabe1168b9253b9562
sdlks1 <135292975+sdlks1@users.noreply.github.com>
2023-06-07 17:42:57 +0800

    Project Outline V0.0-2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from raycast import *
 
 # Initialize
+scene = canvas(width=1024, height=768, center=vec(0,0,0), background=vec(0.6,0.8,0.8), range=15)
+
 for i in range(-30, 31):
-    IMAGE_P.append(box(color=vec(0.6,0.6,0.6)))
+    IMAGE_P.append(image_seg(vec(i*.5,0,0), vec(0,0,0)))
 
 t = 0
 while True:
--- raycast.py
+++ raycast.py
@@ -5,10 +5,24 @@ DT = .01
 IMAGE_P = []
 
 class image_seg:
-    def __init__(self):
-        self.obj = box()
+    def __init__(self, pos, c):
+        self.obj = box(color=c, pos=pos, height=10, width=.5, length=.5)
         self.gc = 0 # Grayscale Value
     def set_color(self):
-        pass
+        self.obj.color = vec(self.gc, self.gc, self.gc)
     def update(self):
-        self.obj = box()
+        pass
+
+class ray:
+    def __init__(self, pos, v, theta):
+        self.obj = sphere(color=vec(1,1,1), pos=pos, size=.25)
+        self.obj.v = vec(v*cos(theta), 0, -v*sin(theta))
+    def collision(self) -> (bool,int):
+        if self.obj.pos.z <= .75:
+            # Collided
+            return True, -1
+        else:
+            return False, -1
+    def update(self):
+        self.obj.pos += self.obj.v*DT
+        self.collision()
