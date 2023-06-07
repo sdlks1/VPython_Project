086bde48575ea86d92992f6ee26647f356287ac9
__DavH__ <135292975+sdlks1@users.noreply.github.com>
2023-06-07 23:37:15 +0800

    Project Reset

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from raycast import *
 
 # Initialize
 scene = canvas(width=1024, height=768, center=vec(0,0,0), background=vec(0.6,0.8,0.8), range=15)
-print("[{} slits\t{} pixels]\n".format(slitN, int(30/image_res)))
-
-print("Rendering\n")
 for i in range(int(-30/image_res*.5), int(30/image_res*.5+1)):
     IMAGE_P.append(image_seg(vec(i*image_res,0,0), vec(0,0,0), i-int(-30/image_res*.5)))  # Normal
     # IMAGE_P.append(image_seg(vec(i*image_res,0,0), vec(.3*i,.59*i,.11*i)))  # Colored Debug
@@ -18,10 +15,10 @@ for i in SLIT:
         j.illumination += vec(c,s,0)
 for i in IMAGE_P:
     # print("Set Pixel["+str(i.index)+"] illumination : "+str(mag(i.illumination)))
-    DBUG("Pixel["+str(i.index)+"] illumination vector "+str(i.illumination.x), str(i.illumination.y))
+    print("Pixel["+str(i.index)+"] illumination vector "+str(i.illumination.x), str(i.illumination.y))
     illu = 1 - (mag(i.illumination)/slitN)
     i.set_color(illu)
-print("Done Rendering\n")
+
 
 # t = 0
 # while True:
---
+++ raycast.py
@@ -0,0 +1,25 @@
+from vpython import *
+from math import *
+
+SHOW_SLIT = False
+
+DT = .01
+image_res = .01  # Image Pixel "Length"
+IMAGE_P = []
+slitN = 1
+slitL = 2
+SLIT = []
+LAMBDA = 1
+
+class image_seg:
+    def __init__(self, pos, c, index):
+        self.obj = box(color=c, pos=pos, height=10, width=.5, length=image_res)
+        self.index = index
+        self.illumination = vec(0, 0, 0)
+    def set_color(self, gc):
+        self.obj.color = vec(gc, gc, gc)
+
+class slit:
+    def __init__(self, pos, index):
+        self.obj = sphere(radius=.5, pos=pos, color=color.red, visible=SHOW_SLIT)
+        self.index = index
