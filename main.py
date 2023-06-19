628e4b4a8c6f8551df99690a889375047d140e70
sdlks1 <135292975+sdlks1@users.noreply.github.com>
2023-06-19 08:11:37 +0800

    Rebuild In-Progress

--- main.py
+++ main.py
@@ -26,26 +26,34 @@ wt_slitN_v = Label(f"{slider_slitN.value()}")
 scene.append_to_caption('\n')
 
 wt_slitL = Label("Slit Spacing")
-slider_slitL = Slider(min=1, max=10, step=.1, length=220, bind=event.push_L)
-wt_slitL_v = Label(f"{slider_slitL.value()}")
-scene.append_to_caption("\n")
+slider_slitL = Slider(min=1, max=1000, step=1, length=220, bind=event.push_L)
+scene.append_to_caption('\t')
+input_slitL = winput(bind=event.push_L_input)
+wt_slitL_v = Label("倍波長")
+scene.append_to_caption('\n')
 
 wt_lambda = Label("Lambda")
 slider_lambda = Slider(min=380, max=780, step=1, length=220, bind=event.push_Lambda)
-wt_lambda_v = Label(f"{slider_lambda.value()}")
+wt_lambda_v = Label(f"{slider_lambda.value()} (nm)")
 scene.append_to_caption('\n')
 
 wt_distance = Label("Distance")
-slider_distance = Slider(min=1e+8, max=1e+9, step=1, length=220, bind=event.push_D)
-wt_distance_v = Label(f"{slider_distance.value()}")
+slider_distance = Slider(min=1, max=1000, step=1, length=220, bind=event.push_D)
+scene.append_to_caption('\t')
+input_distance = winput(bind=event.push_D_input)
+# input_distance = winput(bind=db)
+wt_distance_v = Label("倍波長")
 scene.append_to_caption('\n\n')
 
 btn_simulate = button(text="Simulate", bind=event.push_Sim)
 scene.append_to_caption("\n\n\n")
 
+wt_events = Label(f"{event.buffer}")
+
 
 renderer.render()
 while True:
+    wt_events.modify(f"{event.buffer}")
     if len(event.events) != 0:
         key = event.events[0]
         if key == 'N':
@@ -54,16 +62,23 @@ while True:
             renderer.attributes['N'] = txt
         elif key == 'L':
             txt = slider_slitL.value()
-            wt_slitL_v.modify(txt)
-            renderer.attributes['L'] = txt
+            input_slitL.text = txt
+            renderer.attributes['L'] = txt * renderer.attributes["Lambda"]
+        elif key == "L in":
+            slider_slitL.modify(int(input_slitL.text))
+            renderer.attributes['L'] = int(input_slitL.text)
         elif key == "Lambda":
             txt = slider_lambda.value()
-            wt_lambda_v.modify(txt)
-            renderer.attributes['Lambda'] = txt
+            v = (txt-380) * (9/400) + 1
+            wt_lambda_v.modify(f"{txt} (nm)")
+            renderer.attributes['Lambda'] = v * renderer.attributes["Lambda"]
         elif key == 'D':
             txt = slider_distance.value()
-            wt_distance_v.modify(txt)
-            renderer.attributes['D'] = txt
+            input_distance.text = txt
+            renderer.attributes['D'] = txt * renderer.attributes["Lambda"]
+        elif key == "D in":
+            slider_distance.modify(int(input_distance.text))
+            renderer.attributes['D'] = int(input_distance.text) * renderer.attributes["Lambda"]
         elif key == "sim":
             wt_status.modify("Simulating")
             renderer.simulate()
--- ui.py
+++ ui.py
@@ -10,12 +10,15 @@ class Label:
 class Slider:
     def __init__(self, min, max, step, length, bind):
         self.slider_ = slider(min=min, max=max, step=step, length=length, bind=bind)
+    def modify(self, txt):
+        self.slider_.value = txt
     def value(self):
         return self.slider_.value
 
 class Event:
     def __init__(self):
         self.events = []
+        self.buffer = []
     def push_N(self):
         self.events.append('N')
     def push_L(self):
@@ -26,11 +29,14 @@ class Event:
         self.events.append('D')
     def push_Sim(self):
         self.events.append("sim")
+
+    def push_L_input(self):
+        self.events.append("L in")
+    def push_D_input(self):
+        self.events.append("D in")
+    
     def pop(self):
+        self.buffer.append(self.events[0])
         del self.events[0]
 
 event = Event()
-
-
-
-
